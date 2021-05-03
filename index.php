5f4cb2ad89f06b18a8f96d071e4e1e0807e46285
WiolettaDrag <72939541+WiolettaDrag@users.noreply.github.com>
2021-05-03 18:26:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="pl">
+    <head>
+        <meta charset="utf-8"/>
+        <title>BAZA HR</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+        
+    </head>
+    <body>
+        <header>
+            <p class="main_header">BAZA HR</p>
+        </header>
+		
+       <main class="container">
+	   
+       <a href="dodanie_danych.php"><div>Dodaj nowego pracownika</div></a>
+       <a href="usunięcie_danych.php"><div>Usuń pracownika z bazy</div></a>
+	   <a href="wyświetlanie_danych.php"><div>Wyświetl informacje o pracownikach</div></a>
+          
+       </main>
+    
+       
+    </body>
+</html>
